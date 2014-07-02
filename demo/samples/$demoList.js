956a5d0b71927052d16d9d6a8d5214518a15945b
mlaval <mlaval@amadeus.com>
2014-07-02 14:19:09 +0200

    Generic demo page
    Close #8

---
+++ demo/samples/$demoList.js
@@ -0,0 +1,28 @@
+var asyncRequire = require("noder-js/asyncRequire");
+
+//The list of demos to be loaded is defined here
+var demos = [
+    {id: 'carousel', prettyName: 'Carousel'}
+];
+
+exports.getDemo = function() {
+    var demosTpl = [];
+    for (var i = 0; i < demos.length; i++) {
+        demosTpl.push({
+            id: demos[i].id,
+            prettyName: demos[i].prettyName,
+            demo: require('./' + demos[i].id + '/demo.hsp').demo,
+            description: require('./' + demos[i].id + '/README.hsp').description
+        });
+    }
+    return demosTpl;
+};
+
+exports.getDemo.$preload = function() {
+    var modules = [];
+    for (var i = 0; i < demos.length; i++) {
+        modules.push('./samples/' + demos[i].id + '/demo.hsp');
+        modules.push('./samples/' + demos[i].id + '/README.hsp');
+    }
+    return asyncRequire.apply(null, modules);
+};
